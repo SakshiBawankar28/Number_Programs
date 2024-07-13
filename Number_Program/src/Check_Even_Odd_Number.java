77938139da7f3f41abb2279c37ee4f23c26373b8
SakshiBawankar28 <sakshibawabkar@gmail.com>
2024-07-13 22:04:21 +0530

    Number_Program

---
+++ Number_Program/src/Check_Even_Odd_Number.java
@@ -0,0 +1,22 @@
+//Write a program to display sum of 1 to N numbers?
+
+import java.util.Scanner;
+
+public class Check_Even_Odd_Number {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		int num = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=1; i<=num; i++)
+		{
+			sum += i;
+		}
+		System.out.print("The sum of 1 to "+ num + " is : "+ sum);
+
+	}
+
+}
---
+++ Number_Program/src/Sum_Of_N_Number.java
@@ -0,0 +1,22 @@
+//Write a program to check given number is EVEN or ODD?
+
+import java.util.Scanner;
+
+public class Sum_Of_N_Number {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		int num = sc.nextInt();
+		
+		if(num % 2 == 0)
+		{
+			System.out.println(num + " is an even number");
+		}
+		else
+			System.out.println(num + " is an odd number");
+
+	}
+
+}
